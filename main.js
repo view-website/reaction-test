858e48af9d2286d70c5d4f6301a3f514a78655a9
view-website <91933827+view-website@users.noreply.github.com>
2023-06-28 19:41:53 +0530

    Update main.js
    nothing

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var rand = Math.floor(Math.random() * 5000) + 100;
+var rand = Math.floor(Math.random() * 4000) + 100;
 var c = 0;
 const time = document.getElementById("time");
 var count = 0
+var click = 0
 function start(){
     document.getElementById("not-needed").innerHTML = ++c
     var count = count + 1
@@ -24,10 +25,21 @@ function start(){
     }
 }
 function test(){
-    c = 0;
+     var c = 0;
     tester = setInterval(timing, 10)
     document.getElementById("right").style.backgroundColor = "#66c437"
 }
 function timing(){
+    if( click <= 1 ){
     document.getElementById("time").innerHTML = ++c/100 + " seconds"
+    }
+    else{
+        alert("Click on the light box only once");
+        window.clearInterval(myTimer);
+    }
+}
+function click1(){
+    click = click+1
+    clearInterval(tester)
+    //document.getElementById("time").innerHTML = click;
 }
