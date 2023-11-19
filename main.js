257ee4b0c1255914d696f02d29c0df076e2fcb7a
view-website <91933827+view-website@users.noreply.github.com>
2023-11-19 12:55:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,20 +3,27 @@ var c = 0;
 const time = document.getElementById("time");
 var count = 0
 var click = 0
+var tester1, tester2;
 function restart(){
+    document.getElementById("time").innerHTML = ""
     rand = Math.floor(Math.random() * 4000) + 100;
+    tester1 =0;
+    tester2 = 0;
     c = 0;
     count = 0;
     click = 0;
     document.getElementById("not-needed").innerHTML = ""
-    document.getElementById("time").innerHTML = ""
     document.getElementById("left").style.backgroundColor = "#bbb"
     document.getElementById("mid").style.backgroundColor = "#bbb"
     document.getElementById("mid1").style.backgroundColor = "#bbb"
     document.getElementById("mid2").style.backgroundColor = "#bbb"
     document.getElementById("right").style.backgroundColor = "#bbb"
+    tester1 =0;
+    tester2 = 0;
+    document.getElementById("time").innerHTML = ""
 }
 function start(){
+    document.getElementById("time").innerHTML = ""
     document.getElementById("not-needed").innerHTML = ++c
     var count = count + 1
     document.getElementById("header").innerHTML = "Click anywhere as fast as you can when the lights turns green"
@@ -48,21 +55,24 @@ function test(){
     document.getElementById("mid").style.backgroundColor = "#66c437";
     document.getElementById("mid2").style.backgroundColor = "#66c437";
     document.getElementById("mid1").style.backgroundColor = "#66c437";
-    document.getElementById("left").style.backgroundColor = "#66c437"
+    document.getElementById("left").style.backgroundColor = "#66c437";
+    tester1 = performance.now()
 }
 function timing(){
-    if( click <= 1 ){
-    document.getElementById("time").innerHTML = ++c/100 + " seconds"
+    if( click <= 2 ){
+        //document.getElementById("time").innerHTML = ++c/100 + " seconds"
     }
     else{
-        alert("Click on the light box only once");
+        alert("Click only once. Reload to restart");
         window.clearInterval(myTimer);
     }
 }
 function click1(){
     click = click+1
+    tester2 = performance.now();
+    console.log((tester2-tester1)/1000 + " seconds")
     clearInterval(tester)
     document.getElementById("header").innerHTML = "Restart"
     document.getElementById("header").style.backgroundColor = "#d3d3d3"
-    //document.getElementById("time").innerHTML = click;
+    document.getElementById("time").innerHTML = Math.round((tester2-tester1)*100)/100 + " ms";
 }
